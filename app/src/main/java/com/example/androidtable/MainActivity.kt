f3005d5027cfe5a2350849c9ce2f181daec398c0
Michael He <batmoose@v1040-wn-rt-a-101-117.campus-dynamic.uwaterloo.ca>
2019-09-15 00:10:01 -0400

    Login/Signup is now functional

--- app/src/main/java/com/example/androidtable/MainActivity.kt
+++ app/src/main/java/com/example/androidtable/MainActivity.kt
@@ -6,6 +6,10 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.CheckBox
 import android.widget.EditText
+import android.widget.Toast
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(){
@@ -16,6 +20,8 @@ class MainActivity : AppCompatActivity(){
         supportActionBar?.hide()
         window.setBackgroundDrawableResource(R.drawable.background)
 
+        var fbAuth = FirebaseAuth.getInstance()
+
         var passwordBox = findViewById<EditText>(R.id.password)
         var emailBox = findViewById<EditText>(R.id.email)
         var rememberBox = findViewById<CheckBox>(R.id.remember)
@@ -23,18 +29,39 @@ class MainActivity : AppCompatActivity(){
         var signupButton = findViewById<Button>(R.id.signup)
 
         loginButton.setOnClickListener {
-            var password = passwordBox.text
-            var email = emailBox.text
+            var password = passwordBox.text.toString().trim()
+            var email = emailBox.text.toString().trim()
             var remember = rememberBox.isChecked
+            
+            fbAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener{task: Task<AuthResult> ->
+                if(task.isSuccessful){
+                    Toast.makeText(this@MainActivity, "Login Completed", Toast.LENGTH_SHORT).show()
+                    val intent = Intent(this, MapActivity::class.java)
+                    startActivity(intent)
+                } else {
+                    Toast.makeText(this@MainActivity, "Login failed", Toast.LENGTH_SHORT).show()
+                }
 
-            //TODO: Verify with backend that user info is valid
+            }
 
-            val intent = Intent(this, MapActivity::class.java)
-            // start your next activity
-            startActivity(intent)
+
         }
 
         signupButton.setOnClickListener{
+            var password = passwordBox.text.toString().trim()
+            var email = emailBox.text.toString().trim()
+
+            fbAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener{ task: Task<AuthResult> ->
+                if(task.isSuccessful) {
+                    Toast.makeText(this@MainActivity,"Registration Successful",Toast.LENGTH_SHORT).show()
+                    val intent = Intent(this, MapActivity::class.java)
+                    // start your next activity
+                    startActivity(intent)
+                } else{
+                    Toast.makeText(this@MainActivity, "Registration Failed", Toast.LENGTH_SHORT).show()
+                }
+
+            }
 
         }
     }
